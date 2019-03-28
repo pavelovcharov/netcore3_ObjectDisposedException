c7e4efb44c26af21d90f84f989b331a4566e8ac4
Ovcharov.Pavel <1357165+pavelovcharov@users.noreply.github.com>
2019-03-28 14:42:48 +0300

    init

---
+++ netcore3_ObjectDisposedException/Program.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace netcore3_ObjectDisposedException {
+    static class Program {
+        [TestFixture]
+        public class TestClass {
+            [Test]
+            public void ObjectDisposedEx() {
+                ThreadExceptionEventArgs ea = null;
+                Application.ThreadException += (s, e) => ea = e;
+                Form form = new Form { Size = new Size(500, 400), };
+
+                form.Show();
+                form.Refresh();
+                form.Show(); // second call to actually show form work in debug mode
+
+                var control = new TextBox { Size = new Size(200, 200) };
+                form.Controls.Add(control);
+                control.Dock = DockStyle.Fill;
+                control.Focus();
+                Assert.IsTrue(control.Focused);
+                control.LostFocus += (s, e) => {
+                    control.Dispose();
+                    control = null;
+                };
+                form.Focus();
+                Assert.IsTrue(form.Focused);
+
+                if(ea != null)
+                    Assert.Fail(ea.Exception.StackTrace);
+            }
+        }
+    }
+}
